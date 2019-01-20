96f85889a8e3659d0980287bee45aa855355068a
Giovanni Leo <giovannileo595@gmail.com>
2019-01-20 12:54:03 +0100

    Aggiornamento

--- Sbwt/Sbwt.py
+++ Sbwt/Sbwt.py
@@ -5,16 +5,17 @@ class Sbwt:
     def __init__(self, alphabet):
         self.alphabet = alphabet
         self.alphaUtils = AlphabethUtils(alphabet)
+        self.alphaUtils.perm()
         self.randomAlphabet = self.alphaUtils.getRandomAlphabet()
+
 
     def sbwt(self,s):
         """Apply Burrows-Wheeler transform to input string."""
         assert "$" not in s, "Input string cannot contain STX and ETX characters"
         s = s + "$"  # Add start and end of text marker
-        self.alphaUtils.perm()
         # Table of rotations of string (Viene ordinata secondo l'fabeto custom)
-        matrix = sorted([s[i:] + s[:i] for i in range(len(s))],
-                        key=lambda x: [self.randomAlphabet.index(c) for c in x])
+        print(sorted(s[j:] + s[:j] for j in range(len(s))))
+        matrix = sorted([s[i:] + s[:i] for i in range(len(s))], key=lambda x: [self.randomAlphabet.index(c) for c in x])
         # Table of rotations of string
         last_column = [row[-1:] for row in matrix]  # Last characters of each row
         return "".join(last_column)  # Convert list of characters into string
@@ -24,8 +25,7 @@ class Sbwt:
         table = [""] * len(r)  # Make empty table
         for i in range(len(r)):
             # Add a column of r ((Viene ordinata secondo l'fabeto custom))
-            table = sorted([r[i] + table[i] for i in range(len(r))],
-                           key=lambda x: [self.randomAlphabet.index(c) for c in x])
+            table = sorted([r[i] + table[i] for i in range(len(r))],key=lambda x: [self.randomAlphabet.index(c) for c in x])
         s = [row for row in table if row.endswith("$")][0]  # Find the correct row (ending in ETX)
         return s.strip("$")  # Get rid of start and end markers
 
@@ -34,5 +34,5 @@ if __name__ == "__main__":
     test = Sbwt(alfabeth)
     print(test.sbwt("mississippi"))
     r = test.sbwt("mississippi")
-    print(test.bwt("mississippi"))
+   # print(test.bwt("mississippi"))
     print(test.Sibwt(r))
